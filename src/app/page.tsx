5cb10c56e2c8902d5891b418e3cf57050c1533eb
Jay Cho <jaycho1214@gmail.com>
2025-08-18 14:29:51 +0900

    fix: reverse utility cards order in the grid for improved visibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
       {/* Utility Cards Grid */}
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {utilities.map((utility) => {
+        {utilities.reverse().map((utility) => {
           const IconComponent = utility.icon;
           return (
             <Link
